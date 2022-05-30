75320b8c9c3c539677dc991c6c00faa54d387c1b
Eli Blaney <eliblaney@gmail.com>
2022-05-30 01:32:35 -0500

    Introduce database relationships

--- src/lib.rs
+++ src/lib.rs
@@ -31,14 +31,10 @@ pub fn model(_args: TokenStream, input: TokenStream) -> TokenStream  {
                 ty: Type::Verbatim(quote! { Option<i32> })
             };
             named.insert(0, id_field);
-        } else {
-            return quote! {
-                compile_error!("struct must have named fields");
-            }.into();
         }
     } else {
         return quote! {
-            compile_error!("macro can only be used on structs");
+            compile_error!("macro can only be used on structs with named fields");
         }.into();
     }
 
@@ -64,5 +60,116 @@ pub fn model(_args: TokenStream, input: TokenStream) -> TokenStream  {
 
     quote! {
         compile_error!("can't parse struct");
+    }.into()
+}
+
+fn builder(i: &mut std::vec::IntoIter<proc_macro2::TokenStream>, a: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+    let q = i.next();
+    if q.is_none() { return a; }
+    let q = q.unwrap();
+    return builder(i, quote! {
+        #a
+        #q
+    });
+}
+
+#[proc_macro_derive(Related, attributes(foreign))]
+pub fn impl_related(input: TokenStream) -> TokenStream  {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    if let Data::Struct(s) = &ast.data {
+        if let Fields::Named(f) = &s.fields {
+            let fields = &f.named;
+            let quotes: Vec<_> = fields.into_iter().filter_map(|p| {
+                let attrs = &p.attrs;
+                let attrs: Vec<&Attribute> = attrs.into_iter().filter(|a| {
+                    let segs = &a.path.segments;
+                    segs.len() == 1
+                        && segs.first().unwrap().ident == format_ident!("foreign") 
+                }).collect();
+
+                if attrs.len() != 1 {
+                    return None;
+                }
+
+                let meta = &attrs.first().unwrap().parse_meta().unwrap();
+                let mut obj: Option<String> = None;
+                let mut table: Option<String> = None;
+                let lower_name = &name.to_string().to_lowercase();
+                let mut lower_name_ident = format_ident!("{}", &lower_name);
+
+                if let Meta::List(ml) = &meta {
+                    ml.nested.iter().for_each(|m| {
+                        if let NestedMeta::Meta(inner) = m {
+                            if let Meta::NameValue(nv) = inner {
+                                let ident = &nv.path.segments.first().unwrap().ident;
+                                if ident == &format_ident!("type") {
+                                    if let Lit::Str(s) = &nv.lit {
+                                        obj = Some(s.value());
+                                    }
+                                }
+                                if ident == &format_ident!("table") {
+                                    if let Lit::Str(s) = &nv.lit {
+                                        table = Some(s.value())
+                                    }
+                                }
+                                if ident == &format_ident!("collect") {
+                                    if let Lit::Str(s) = &nv.lit {
+                                        lower_name_ident = format_ident!("{}", s.value())
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+
+                if obj.is_none() || table.is_none() {
+                    return quote! {
+                            compile_error!("foreign attribute must include type and table");
+                        }.into();
+                }
+
+                let obj = obj.unwrap();
+                let table = table.unwrap();
+                let obj = format_ident!("{}", &obj);
+                let field = &p.ident.as_ref().unwrap();
+                let field_string: &str = &field.to_string();
+                let shortened_field = &field_string.split("_").next().unwrap();
+                let fname = format_ident!("get_{}", &shortened_field);
+                let q = quote! {
+                    impl #name {
+                        pub async fn #fname(&self, mut db: rocket_db_pools::Connection<super::Db>) -> #obj {
+                            use rocket::futures::TryFutureExt;
+                            rocket_db_pools::sqlx::query("SELECT * FROM $1 WHERE id = $2")
+                                .bind(#table).bind(&self.#field)
+                                .fetch_one(&mut *db)
+                                .map_ok(|r| <#obj>::from(r))
+                                .await.ok().unwrap()
+                        }
+                    }
+
+                    impl #obj {
+                        pub async fn #lower_name_ident(&self, mut db: rocket_db_pools::Connection<super::Db>) -> Vec<#name> {
+                            use rocket::futures::TryStreamExt;
+                            rocket_db_pools::sqlx::query("SELECT * FROM $1 WHERE $1.$2 = $3")
+                                .bind(#lower_name).bind(#field_string).bind(&self.id)
+                                .fetch(&mut *db)
+                                .map_ok(|r| <#name>::from(r))
+                                .try_collect::<Vec<_>>()
+                                .await.ok().unwrap()
+                        }
+                    }
+
+                };
+
+                return Some(q);
+            }).collect();
+
+            return builder(&mut quotes.into_iter(), quote! {}).into();
+        }
+    }
+
+    quote! {
+        compile_error!("macro can only be used on structs with named fields");
     }.into()
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 use rocket::serde::{Deserialize, Serialize};
-use memra::model;
+use chrono::NaiveDateTime;
+use memra::{model, Related};
 
 #[model]
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -11,10 +12,132 @@ pub struct User {
 }
 
 #[model]
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Credentials {
+    #[foreign(type="User", table="users")]
     pub user_id: i32,
     pub email: String,
     pub password: String,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Course {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    pub visibility: Option<bool>,
+    pub name: String,
+    pub image: Vec<u8>,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Deck {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    pub visibility: Option<bool>,
+    pub name: String,
+    pub image: Vec<u8>,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Card {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    #[foreign(type="Deck", table="decks")]
+    pub deck_id: i32,
+    pub front: Vec<u8>,
+    pub back: Vec<u8>,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct History {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    #[foreign(type="Card", table="cards")]
+    pub card_id: i32,
+    pub ts: NaiveDateTime,
+    pub num_confident: i32,
+    pub num_correct: i32,
+    pub num_wrong: i32,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Settings {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    pub public_profile: Option<bool>,
+    pub avatar: Vec<u8>,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Notification {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    pub ts: NaiveDateTime,
+    pub message: String,
+    pub icon: Vec<u8>,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Addon {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    pub visibility: Option<bool>,
+    pub name: String,
+    pub description: String,
+    pub data: Vec<u8>,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct CourseDeck {
+    #[foreign(type="Course", table="courses")]
+    pub course_id: i32,
+    #[foreign(type="Deck", table="decks")]
+    pub deck_id: i32,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Followers {
+    #[foreign(type="User", table="users", collect="followers")]
+    pub follower_id: i32,
+    #[foreign(type="User", table="users", collect="following")]
+    pub following_id: i32,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct CourseSubscription {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    #[foreign(type="Course", table="courses")]
+    pub course_id: i32,
+}
+
+#[model]
+#[derive(Related, Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct DeckSubscription {
+    #[foreign(type="User", table="users")]
+    pub user_id: i32,
+    #[foreign(type="Course", table="courses")]
+    pub deck_id: i32,
 }
