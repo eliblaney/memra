77bc14607c3272acb7ea69d4fbf37e994745de9b
Eli Blaney <eliblaney@gmail.com>
2022-06-02 11:18:21 -0500

    Add home page template component

---
+++ app/src/pages/home/home.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './style.css';
+
+function Home() {
+	return (
+		<div className="App">
+			<h1>Home</h1>
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+		</div>
+	);
+}
+
+export default Home;
